b326a7d6e7f3682467b7c2c8412a6d7cef756809
subhonbek <basubhonbek@gmail.com>
2024-02-01 16:55:44 +0300

    deploy

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,14 +1,14 @@
-import { buildConfig } from "payload/config";
+import dotenv from "dotenv";
 import { webpackBundler } from "@payloadcms/bundler-webpack";
 import { mongooseAdapter } from "@payloadcms/db-mongodb";
+import { Users } from "./collections/Users";
 import { slateEditor } from "@payloadcms/richtext-slate";
 import path from "path";
-import { Users } from "./collections/Users";
-import dotenv from "dotenv";
 import { Products } from "./collections/Products/Products";
 import { Media } from "./collections/Media";
 import { ProductFiles } from "./collections/ProductFile";
 import { Orders } from "./collections/Orders";
+import { buildConfig } from "payload/config";
 
 dotenv.config({
   path: path.resolve(__dirname, "../.env"),
